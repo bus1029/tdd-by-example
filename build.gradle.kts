37f70e29dc52d653ae51bf626d363fe8cc8eabab
Seokhyun Bae <bus1029@gmail.com>
2022-02-22 20:23:32 +0900

    feat: Money 객체의 times 메소드에 대한 테스트 코드 작성
    - 다중통화를 지원하는 Money 객체를 생성하고 해당 객체에서 
    주가와 주식수를 곱한 금액을 결과로 반환하는 times 메소드에 대한
    테스트 코드 작성
    - 테스트 코드를 통과하는 간단한 코드 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.22")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.22.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
---
+++ src/test/java/currency/MoneyTest.java
@@ -0,0 +1,20 @@
+package currency;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MoneyTest {
+  // 1. 작은 테스트를 하나 추가한다.
+  // 2. 모든 테스트를 실행해서 테스트가 실패하는 것을 확인한다.
+  // 3. 조금 수정한다.
+  // 4. 모든 테스트를 실행해서 테스트가 성공하는 것을 확인한다.
+  // 5. 중복을 제거하기 위해 리팩토링을 한다.
+
+  @Test
+  void testMultiplication() {
+    Dollar five = new Dollar(5);
+    five.times(2);
+    assertThat(five.amount).isEqualTo(10);
+  }
+}
