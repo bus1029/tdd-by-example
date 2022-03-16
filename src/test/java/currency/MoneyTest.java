d67d18a57bb46cc3f8e8964c777ac73d31929933
Seokhyun Bae <bus1029@gmail.com>
2022-03-16 19:25:58 +0900

    feat: 환율을 적용시킬 수 있도록 수정
    - Bank가 환율에 따라 통화 교환을 담당할 수 있도록 reduce에 Bank
    객체를 넘겨주도록 수정
    - Interface에 정의된 Method의 시그니처가 변했기 때문에 Money 및 
    Sum의 reduce도 Bank를 넘겨받도록 처리
      - Money의 경우 환율에 따라 통화의 비율이 달라지도록 rate 메소드를
      reduce 내부에서 호출하여 결과값을 처리하도록 수정
      - Sum의 경우 아직 테스트 코드가 작성되지 않았기 때문에 무시
    - 환율에 따른 교환 비를 저장하기 위해 Bank 클래스에 HashMap
    형태의 자료구조 선언 및 addRate 메소드 추가
    -  HashMap에서 값을 찾을 수 있도록 rate 메소드 추가
    
    TDD 14. 바꾸기

--- src/test/java/currency/MoneyTest.java
+++ src/test/java/currency/MoneyTest.java
@@ -69,4 +69,17 @@ class MoneyTest {
     Money result = bank.reduce(Money.dollar(1), "USD");
     assertThat(result).isEqualTo(Money.dollar(1));
   }
+
+  @Test
+  void testReduceMoneyDifferentCurrency() {
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Money result = bank.reduce(Money.franc(2), "USD");
+    assertThat(result).isEqualTo(Money.dollar(1));
+  }
+
+  @Test
+  void testIdentityRate() {
+    assertThat(new Bank().rate("USD", "USD")).isEqualTo(1);
+  }
 }
