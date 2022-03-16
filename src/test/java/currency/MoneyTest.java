d1b36b0da1f0effc9609b5bda5d3d8e06227c802
Seokhyun Bae <bus1029@gmail.com>
2022-03-16 20:06:29 +0900

    feat: 연산끼리 더하기, 곱하기 구현
    - 연산끼리 더하고, 곱하기를 구현하기 위해 Expression에 times 선언
    - Money에는 times가 default 공개자로 설정되어 있기 때문에 public
    으로 수정
    - Sum에는 stub으로 구현되어 있던 plus를 구현하고, 
    Expression에 선언된 times 또한 구현
    - 테스트에서 fiveBucks와 tenFrancs를 직접 더해서 Sum을 생성할 수도
    있지만, 명시적으로 Sum을 생성하여 직접적으로 테스트의 의도를
    드러냈다.
    
    TDD 16. 드디어, 추상화

--- src/test/java/currency/MoneyTest.java
+++ src/test/java/currency/MoneyTest.java
@@ -92,4 +92,26 @@ class MoneyTest {
     Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
     assertThat(result).isEqualTo(Money.dollar(10));
   }
+
+  @Test
+  void testSumPlusMoney() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFrancs = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+    Money result = bank.reduce(sum, "USD");
+    assertThat(result).isEqualTo(Money.dollar(15));
+  }
+
+  @Test
+  void testSumTimes() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFrancs = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
+    Money result = bank.reduce(sum, "USD");
+    assertThat(result).isEqualTo(Money.dollar(20));
+  }
 }
